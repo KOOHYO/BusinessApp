569a6a8de9bc080a28b35e92a70f73dd8013adf0
KOOHYO <zpfhzpfh112@naver.com>
2023-08-29 18:56:14 +0900

    프로젝트 생성

---
+++ BusinessApp/src/main/java/com/busi/ness/BusinessAppApplication.java
@@ -0,0 +1,13 @@
+package com.busi.ness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessAppApplication.class, args);
+	}
+
+}
