f6ed4822145a3887d3cb1ccb1b76cf9b8310d977
KOOHYO <zpfhzpfh112@naver.com>
2023-08-30 10:27:19 +0900

    DB사용 제외

--- BusinessApp/src/main/java/com/busi/ness/BusinessAppApplication.java
+++ BusinessApp/src/main/java/com/busi/ness/BusinessAppApplication.java
@@ -2,8 +2,9 @@ package com.busi.ness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class BusinessAppApplication {
 
 	public static void main(String[] args) {
