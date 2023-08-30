2302272c467266a900e94e1150f776d4abb3dfe7
KOOHYO <zpfhzpfh112@naver.com>
2023-08-30 18:01:27 +0900

    템플릿 적용 완료, 마이바티스 연동중

---
+++ BusinessApp/src/main/java/com/busi/ness/HomeController.java
@@ -0,0 +1,19 @@
+package com.busi.ness;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+public class HomeController {
+
+	@GetMapping(value = "/")
+	public ModelAndView main() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+	
+}
